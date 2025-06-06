68e5218353b900369dd716e100c9e9f500bdd5c2
manuelgarciacr <29376434+manuelgarciacr@users.noreply.github.com>
2025-06-06 15:43:10 +0200

    Auto-generated commit

---
+++ main-2K7MSPYL.js
@@ -0,0 +1,1 @@
+import{b as o,e as p,g as r}from"./chunk-HGFEUVXO.js";import"./chunk-XUQKW7F4.js";o(p,r).catch(t=>console.error(t));
--- main-PQRPSNFN.js
+++
@@ -1,1 +0,0 @@
-import{b as o,e as p,g as r}from"./chunk-UJ5IJTB4.js";import"./chunk-XUQKW7F4.js";o(p,r).catch(t=>console.error(t));
